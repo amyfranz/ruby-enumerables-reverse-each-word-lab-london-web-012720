fe1265699077e0de65c7d8640e73754e14a82032
Amy Franz <amy.franz@gmail.com>
2020-01-10 16:01:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,11 @@ def reverse_each_word(sentence)
   sentence = ''
   first_array.each do |word|
     word.reverse!
-    sentence = "#{sentence} #{word}"
+    if sentence == ''
+      sentence = "#{word}"
+    else
+      sentence = "#{sentence} #{word}"
+    end
   end
   return sentence
 end
